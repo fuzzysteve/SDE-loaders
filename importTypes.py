6379a554e79851661e1a53dd482875426e619fdc
Steve <steve.ronuken@fuzzwork.co.uk>
2015-07-10 20:13:33 +0100

    cleaner syntax
    changed the syntax for the loader to one suggested by Nullabor (someone
    might think he uses python professionally) and forced an encoding onto
    the strings going in for names.

--- importTypes.py
+++ importTypes.py
@@ -24,26 +24,25 @@ with open('typeIDs.yaml','r') as yamlstream:
     typeids=yaml.load(yamlstream,Loader=yaml.CSafeLoader)
     print "Yaml Processed into memory"
     for typeid in typeids:
-        print typeid
         connection.execute(invTypes.insert(),
                            typeID=typeid,
-                           groupID=typeids[typeid]['groupID'] if typeids[typeid].has_key('groupID') else 0,
-                           typeName=typeids[typeid]['name']['en'] if (typeids[typeid].has_key('name') and typeids[typeid]['name'].has_key('en')) else '',
-                           description=typeids[typeid]['description']['en'] if (typeids[typeid].has_key('description') and typeids[typeid]['description'].has_key('en')) else '',
-                           mass=typeids[typeid]['mass'] if typeids[typeid].has_key('mass') else 0,
-                           volume=typeids[typeid]['volume'] if typeids[typeid].has_key('volume') else 0,
-                           capacity=typeids[typeid]['capacity'] if typeids[typeid].has_key('capacity') else 0,
-                           portionSize=typeids[typeid]['portionSize'] if typeids[typeid].has_key('portionSize') else 0,
-                           raceID=typeids[typeid]['raceID'] if typeids[typeid].has_key('raceID') else 0,
-                           basePrice=typeids[typeid]['basePrice'] if typeids[typeid].has_key('basePrice') else 0,
-                           published=typeids[typeid]['published'] if typeids[typeid].has_key('published') else 0,
-                           marketGroupID=typeids[typeid]['marketGroupID'] if typeids[typeid].has_key('marketGroupID') else 0,
-                           iconID=typeids[typeid]['iconID'] if typeids[typeid].has_key('iconID') else 0,
-                           soundID=typeids[typeid]['soundID'] if typeids[typeid].has_key('soundID') else 0)
+                           groupID=typeids[typeid].get('groupID',0),
+                           typeName=typeids[typeid].get('name',{}).get('en','').decode('utf-8'),
+                           description=typeids[typeid].get('description',{}).get('en','').decode('utf-8'),
+                           mass=typeids[typeid].get('mass',0),
+                           volume=typeids[typeid].get('volume',0),
+                           capacity=typeids[typeid].get('capacity',0),
+                           portionSize=typeids[typeid].get('portionSize'),
+                           raceID=typeids[typeid].get('raceID'),
+                           basePrice=typeids[typeid].get('basePrice'),
+                           published=typeids[typeid].get('published',0),
+                           marketGroupID=typeids[typeid].get('marketGroupID'),
+                           iconID=typeids[typeid].get('iconID'),
+                           soundID=typeids[typeid].get('soundID'))
         if (typeids[typeid].has_key('name')):
             for lang in typeids[typeid]['name']:
-                connection.execute(trnTranslations.insert(),tcID=8,keyID=typeid,languageID=lang,text=typeids[typeid]['name'][lang]);
+                connection.execute(trnTranslations.insert(),tcID=8,keyID=typeid,languageID=lang,text=typeids[typeid]['name'][lang].decode('utf-8'));
         if (typeids[typeid].has_key('description')):
             for lang in typeids[typeid]['description']:
-                connection.execute(trnTranslations.insert(),tcID=33,keyID=typeid,languageID=lang,text=typeids[typeid]['description'][lang]);
+                connection.execute(trnTranslations.insert(),tcID=33,keyID=typeid,languageID=lang,text=typeids[typeid]['description'][lang].decode('utf-8'));
 trans.commit()
